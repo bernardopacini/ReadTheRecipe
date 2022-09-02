fedcbfb3ea5d3d13ae0ebb3645187b6bb806f1a6
Bernardo Pacini <61238730+bernardopacini@users.noreply.github.com>
2022-09-01 23:11:36 -0400

    Moving to Customized Theme (#29)
    * Moving to a new theme
    
    * Minor updates to README
    
    * Fixing theme package capitalization
    
    * Fixing issues with dashes and underscores in theme name

--- source/conf.py
+++ source/conf.py
@@ -13,12 +13,13 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sphinxReadTheRecipeTheme
 
 
 # -- Project information -----------------------------------------------------
 
 project = "ReadTheRecipe"
-copyright = "2021"
+copyright = "2022"
 author = ""
 
 
@@ -27,7 +28,7 @@ author = ""
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme"]
+# extensions = ["sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -43,7 +44,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinxReadTheRecipeTheme"
+html_theme_path = [sphinxReadTheRecipeTheme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
