691c7962d582f87b3585b714ec15e1dd39701b0e
Bernardo Pacini <bpacini@umich.edu>
2022-09-01 23:04:52 -0400

    Fixing issues with dashes and underscores in theme name

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_ReadTheRecipe_theme
+import sphinxReadTheRecipeTheme
 
 
 # -- Project information -----------------------------------------------------
@@ -44,8 +44,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_ReadTheRecipe_theme"
-html_theme_path = [sphinx_ReadTheRecipe_theme.get_html_theme_path()]
+html_theme = "sphinxReadTheRecipeTheme"
+html_theme_path = [sphinxReadTheRecipeTheme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
